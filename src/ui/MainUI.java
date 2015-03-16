52dcaaca21510f83f6ada6d11aedff9ac78c91c3
Sharmaine Swee <sweesl1993@gmail.com>
2015-03-16 23:24:13 +0800

    added MainUI for windowsBuilder

---
+++ src/ui/MainUI.java
@@ -0,0 +1,56 @@
+package ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
+
+public class MainUI {
+
+	private JFrame frame;
+	private JTable table;
+	private JTable table_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainUI window = new MainUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.WHITE);
+		frame.setBounds(100, 100, 608, 384);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.getContentPane().add(table, BorderLayout.CENTER);
+		
+		table = new JTable();
+		table.setBackground(Color.LIGHT_GRAY);
+		frame.getContentPane().add(table, BorderLayout.CENTER);
+	}
+
+}
