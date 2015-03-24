928ea3b7fd7c2071d83ba01fb0db182ecf41163f
luckyxkw <cocolonel.x@gmail.com>
2015-03-24 19:52:26 +0800

    add unit tests to DateParser

--- src/parser/DateParser.java
+++ src/parser/DateParser.java
@@ -7,7 +7,8 @@ import com.joestelmach.natty.*;
 
 public class DateParser {
 	private com.joestelmach.natty.Parser dateParser = null;
-	
+	private String[] dateIndicators = {"/", "-"};
+	private String[] timeIndicators = {".", ":"};
 	public DateParser(){
 		dateParser = new com.joestelmach.natty.Parser();
 	}
@@ -16,11 +17,37 @@ public class DateParser {
 		if (dateString == null) {
 			throw new NullPointerException("the command cannot be null");
 		}
+		
 		List<DateGroup> groups =  dateParser.parse(dateString);
 		if (groups.isEmpty()) {
 			return null;
 		} else {
 			return groups.get(0).getDates().get(0);
 		}
+	}
+	
+	private String getDateElement(int intendedPotion, String dateString) {
+		assert(dateString != null);
+		for (String temp: dateIndicators) {
+			if (dateString.contains(temp)) {
+				int count = 0;
+				int start = 0;
+				while count < intended
+				break;
+			}
+		}
+		
+	}
+	
+	private String getTimeElement(int intendedPotion, String dateString) {
+		assert(dateString != null);
+		for (String temp: dateIndicators) {
+			if (dateString.contains(temp)) {
+				int count = 0;
+				
+				break;
+			}
+		}
+		
 	}
 }
--- src/parser/testDateParser.java
+++ src/parser/testDateParser.java
@@ -2,6 +2,8 @@ package parser;
 
 import static org.junit.Assert.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.junit.Test;
@@ -10,9 +12,95 @@ public class testDateParser {
 
 	@Test
 	public void test() {
+		
+		Date output;
 		DateParser dp = new DateParser();
-		Date output = dp.getDate(null);
-		System.out.println(output);
+		SimpleDateFormat sdf;
+		String dateString;
+		//test null input
+		try{
+			output = dp.getDate(null);
+			fail("No exception thrown.");
+		}catch(Exception ex){
+			assertTrue(ex instanceof NullPointerException);
+			assertTrue(ex.getMessage().contains("the command cannot be null"));
+		}
+		
+		//test empty input
+		output = dp.getDate("");
+		assertEquals(null, output);
+		
+		//test date string with illegal date flat/leap
+		try {
+			sdf = new SimpleDateFormat("yyyy-MM-dd");
+			dateString = "2015-2-29";
+			output = dp.getDate(dateString);
+			dateString = "2015-3-1";
+			assertEquals(sdf.parse(dateString), sdf.parse(sdf.format(output)));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			sdf = new SimpleDateFormat("yyyy-MM-dd");
+			dateString = "2012-2-29";
+			output = dp.getDate(dateString);
+			dateString = "2012-2-29";
+			assertEquals(sdf.parse(dateString), sdf.parse(sdf.format(output)));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		//test date string with illegal date month illegal
+		try {
+			sdf = new SimpleDateFormat("yyyy-MM-dd");
+			dateString = "2015-13-29";
+			output = dp.getDate(dateString);
+			dateString = "2015-3-1";
+			assertEquals(sdf.parse(dateString), sdf.parse(sdf.format(output)));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		//test date only string yyyy-MM-dd
+		try {
+			sdf = new SimpleDateFormat("yyyy-MM-dd");
+			dateString = "2015-4-25";
+			output = dp.getDate(dateString);
+			assertEquals(sdf.parse(dateString), sdf.parse(sdf.format(output)));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		//test date only string yyyy/MM/dd
+		try {
+			sdf = new SimpleDateFormat("yyyy/MM/dd");
+			dateString = "2015/4/25";
+			output = dp.getDate(dateString);
+			assertEquals(sdf.parse(dateString), sdf.parse(sdf.format(output)));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		//test date only string MM/dd/yyyy
+		try {
+			sdf = new SimpleDateFormat("MM/dd/yyyy");
+			dateString = "4/25/2015";
+			output = dp.getDate(dateString);
+			assertEquals(sdf.parse(dateString), sdf.parse(sdf.format(output)));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		//test string with both time and date
+		try {
+			sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+			dateString = "2015-4-25 13:00";
+			output = dp.getDate(dateString);
+			assertEquals(sdf.parse(dateString), output);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
