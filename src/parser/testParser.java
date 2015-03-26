045bc32510aca17bab31a239508ae2977ef0ae94
luckyxkw <cocolonel.x@gmail.com>
2015-03-26 10:56:55 +0800

    standardise unit test for parser and dateparser

--- src/parser/testParser.java
+++ src/parser/testParser.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import java.io.IOException;
 import java.util.Date;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class testParser {
@@ -16,14 +18,22 @@ public class testParser {
 	private static final int OPERATION_DISPLAY = 4;
 	private static final int OPERATION_EXIT = 5;
 	private static final int OPERATION_MODIFY = 6;
+	private static final int OPERATION_UNDO = 7;
+	private static final int OPERATION_REDO = 8;
+	private static final int OPERATION_SORT = 9;
+	private static final int OPERATION_SEARCH = 10;
+	private boolean testBoolean;
+	private int testNumber;
+	private String testString;
+	private Parser p;
+	
+	@Before
+	public void initParser() {
+		p = new Parser();
+	}
 	
 	@Test
-	public void test() {
-		Parser p = new Parser();
-		boolean testBoolean;
-		int testNumber;
-		String testString;
-		
+	public void testArguments() {
 		//test Arguments normal
 		testBoolean = p.isArgumentsCorrect("add have lessons -d tomorrow -v school");
 		assertEquals(true, testBoolean);
@@ -33,7 +43,10 @@ public class testParser {
 		//test Arguments unknown arguments
 		testBoolean = p.isArgumentsCorrect("add this is for fun -cs dou wo");
 		assertEquals(false, testBoolean);
-		
+	}
+	
+	@Test
+	public void testGetIndex() {
 		//test getIndex normal
 		try {
 			testNumber = p.getIndex("modify 7 -d the day after tomorrow");
@@ -55,7 +68,10 @@ public class testParser {
 			assertTrue(e instanceof IOException);
 			assertTrue(e.getMessage().contains("the index you entered is illegal"));
 		}
-		
+	}
+	
+	@Test
+	public void testGetNewTitle() {
 		//test getNewTitle normal
 		testString = p.getNewTitle("modify 8 go to school -d tomorrow");
 		assertEquals("go to school", testString);
@@ -64,5 +80,9 @@ public class testParser {
 		assertEquals(null, testString);
 	}
 	
+	@After
+	public void cleanUp() {
+		p = null;
+	}
 	
 }
