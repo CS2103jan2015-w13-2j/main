cb7fac166e90083afa49ddd9b144a57b5d6c3da8
luckyxkw <cocolonel.x@gmail.com>
2015-03-30 11:18:08 +0800

    add mark complete in parser

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -44,22 +44,6 @@ public class Parser {
 	private static final String EXCEPTION_NOINDEX = "you must enter an index";
 	private static final String EXCEPTION_NULLPOINTER = "The command is null";
 	
-	private static final String REGEX_VENUE = "[ at | in ]?.*";
-	private static final String REGEX_TIME = "[ at | on ]?.*";
-	private static final String REGEX_DEADLINE = "[ by | due on | due at ]?.*";
-	private static final String REGEX_ADD = "\".+\"\\s*.*";
-	private static final String REGEX_DELETE = "[delete|rm|remove]\\s*[0..9]*";
-	private static final String REGEX_CLEAR = "[clear|clean]";
-	private static final String REGEX_DISPLAY = "[display|ls|show]";
-	private static final String REGEX_EXIT = "[exit|quit]";
-	private static final String REGEX_MODIFY = "(update|modify)\\s*[0..9]+\\s+.+\\s+"+
-												REGEX_VENUE+"\\s+"+REGEX_TIME;
-	private static final String REGEX_UNDO = "[undo]";
-	private static final String REGEX_REDO = "[redo]";
-	private static final String REGEX_SEARCH = "[search]\\s+.*";
-	private static final String REGEX_COLON = "\"";
-	private static final String REGEX_INDICATOR = "( on | at | in )";
-
 	private static final int LARGE_CONSTANT = 500;
 	private static final int FAIL = -1;
 	private static final int OPERATION_UNKNOWN = 0;
@@ -73,6 +57,7 @@ public class Parser {
 	private static final int OPERATION_REDO = 8;
 	private static final int OPERATION_SORT = 9;
 	private static final int OPERATION_SEARCH = 10;
+	private static final int OPERATION_COMPLETE = 11;
 	
 	private static final String[] KEYWORD_ADD = {"add", "insert"};
 	private static final String[] KEYWORD_DELETE = {"delete", "remove", "rm"};
@@ -83,23 +68,12 @@ public class Parser {
 	private static final String[] KEYWORD_UNDO = {"undo"};
 	private static final String[] KEYWORD_REDO = {"redo"};
 	private static final String[] KEYWORD_SORT = {"sort"};
-	private static final String[] KEYWORD_SEARCH = {"find","search"};
+	private static final String[] KEYWORD_SEARCH = {"find", "search"};
+	private static final String[] KEYWORD_COMPLETE = {"finish", "complete"};
 	
 	private static final String[] OPTIONS = {"-v", "-d", "-dd", "-c"};
 	
 	private static final Pattern NUMBERS = Pattern.compile(".*[^0-9].*");
-	private static final Pattern COMMAND_ADD_TIME = Pattern.compile(REGEX_ADD+REGEX_VENUE+REGEX_TIME);
-	private static final Pattern COMMAND_ADD_DEAD = Pattern.compile(REGEX_ADD+REGEX_VENUE+REGEX_DEADLINE);
-	private static final Pattern COMMAND_DELETE = Pattern.compile(REGEX_DELETE);
-	private static final Pattern COMMAND_CLEAR = Pattern.compile(REGEX_CLEAR);
-	private static final Pattern COMMAND_DISPLAY = Pattern.compile(REGEX_DISPLAY);
-	private static final Pattern COMMAND_MODIFY = Pattern.compile(REGEX_MODIFY);
-	private static final Pattern COMMAND_SEARCH = Pattern.compile(REGEX_SEARCH);
-	private static final Pattern COMMAND_UNDO = Pattern.compile(REGEX_UNDO);
-	private static final Pattern COMMAND_REDO = Pattern.compile(REGEX_REDO);
-	private static final Pattern COMMAND_EXIT = Pattern.compile(REGEX_EXIT);
-	private static final Pattern SPLIT_COLON = Pattern.compile(REGEX_COLON);
-	private static final Pattern SPLIT_INDICATOR = Pattern.compile(REGEX_INDICATOR);
 	
 	private static Hashtable<String, Integer> featureList = null; 
 	private static DateParser dateParser = null;
@@ -111,27 +85,7 @@ public class Parser {
 		initFeatureList();
 		dateParser = new DateParser();
 	}
-	
-	public ArrayList<String> check(String str) {
-		Matcher m = COMMAND_ADD_TIME.matcher(str);
-		ArrayList<String> temp = new ArrayList<String>();
-		if (m.matches()) {
-			String[] strings = SPLIT_COLON.split(str);
-			temp.add(strings[1]);
-			strings = SPLIT_INDICATOR.split(str);
-			if (strings.length>1) {
-				int index = 1;
-				while (index<strings.length) {
-					temp.add(strings[index]);
-					index++;
-				}
-			}
-			return temp;
-		} else {
-			return null;
-		}
-	}
-	
+
 	public int getOperation(String operation) throws NullPointerException {
 		if (operation == null) {
 			logNullPointer(EXCEPTION_NULLPOINTER);
@@ -333,6 +287,7 @@ public class Parser {
 		addSelectedFeature(KEYWORD_REDO, OPERATION_REDO);
 		addSelectedFeature(KEYWORD_SORT, OPERATION_SORT);
 		addSelectedFeature(KEYWORD_SEARCH, OPERATION_SEARCH);
+		addSelectedFeature(KEYWORD_COMPLETE, OPERATION_COMPLETE);
 	}
 	
 	private void addSelectedFeature(String[] keyword, Integer operation) {
