7bf80cfd5296767795520c08132a3c7da4606a95
luckyxkw <cocolonel.x@gmail.com>
2015-04-12 14:39:57 +0800

    change formatDefault and isSameDay to static

--- src/parser/DateParser.java
+++ src/parser/DateParser.java
@@ -50,7 +50,7 @@ public class DateParser {
 		}
 	}
 	
-	public String formatDefault(Date date) throws  NullPointerException {
+	public static String formatDefault(Date date) throws  NullPointerException {
 		if (date == null) {
 			throw new NullPointerException(EXCEPTION_NULLPOINTER);
 		}
@@ -58,7 +58,7 @@ public class DateParser {
 		return dateFormat.format(date);
 	}
 	
-	public boolean isSameDay(Date d1, Date d2) throws NullPointerException {
+	public static boolean isSameDay(Date d1, Date d2) throws NullPointerException {
 		if (d1 == null || d2 == null) {
 			throw new NullPointerException(EXCEPTION_NULLPOINTER);
 		}
